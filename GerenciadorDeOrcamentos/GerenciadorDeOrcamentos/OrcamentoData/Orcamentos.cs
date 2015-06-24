bf6c5430b0be7b7cc0e8ab63d19b705523d1085d
MauricioBuzata <mauricio_buzata@hotmail.com>
2015-06-23 21:14:41 -0300

    atualizacoes Data

--- GerenciadorDeOrcamentos/GerenciadorDeOrcamentos/OrcamentoData/Orcamentos.cs
+++ GerenciadorDeOrcamentos/GerenciadorDeOrcamentos/OrcamentoData/Orcamentos.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OrcamentoData
 {
     public class Orcamentos
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdOrcamento { get; set; }
         public Clientes Cliente { get; set; }
-        public List<Produtos> Produto { get; set; }
+        //public List<Produtos> Produto { get; set; }
+        public decimal TotalOrcamento { get; set; }
     }
 }
